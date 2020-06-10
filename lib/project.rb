65b60886ba8acf5183eda47c804a4186b356c5eb
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 18:13:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,8 +9,7 @@ class Project
   
   def add_backer(name)
     @backers << name
-    name.backed_projects << self unless name.backed_projects == self
-    backer.backed_projects << self
+    name.backed_projects << self unless name.backed_projects.inclkude?(self)
   end
   
 end
