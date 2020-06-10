6db543e0fec28a907f3dcbd652f117db188dcc18
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 18:05:20 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,14 @@
 class Backer
+  attr_accessor :name
+  attr_reader :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+  
+  end
   
 end
